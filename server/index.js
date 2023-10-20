90570bbc0c1f859b713fbee916862c28e272a384
s317631 <s317631@studenti.polito.it>
2023-10-20 17:33:26 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -181,4 +181,4 @@ app.delete('/api/sessions/current', (req, res) => {
 
 
 const PORT = 3001;
-app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}`));
