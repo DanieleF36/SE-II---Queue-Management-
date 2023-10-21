65a601df7f2ee5cd320dcfabdec1767de401fd63
s317631 <s317631@studenti.polito.it>
2023-10-21 16:41:06 +0200

    Database Config

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(express.static('public'))
 
 const corsOptions = {
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true,
 };
 app.use(cors(corsOptions));
@@ -51,6 +51,8 @@ const isLoggedIn = (req, res, next) => {
     return res.status(401).json({ error: 'Not authenticated'});
 }
 const answerDelay = 300;
+//Number of total counter, usable for checking
+const nCounter = 3;
 
 app.use(session({
     secret:'anjndaljjahuiq8989',
@@ -61,6 +63,96 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/api/pages/:id', [
+    check('id').isInt(),
+], async (req, res)=>{
+    try {
+        let result = await dao.getPage(req.params.id);
+        let paragraph = await dao.getParagraphs(req.params.id);
+        let images = await dao.getImages(req.params.id);
+        let headers = await dao.getHeaders(req.params.id);
+        let block = paragraph.concat(images);
+        block = block.concat(headers);
+        result.order= block.sort((a, b) => a.pos - b.pos );
+        if(result.error)
+            res.status(404).json(result);
+        else
+            res.status(200).json(result);
+    } catch(err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+app.post('/api/auth/pages', isLoggedIn, [
+    check('title').isLength({min:1}),
+    check('creationDate').isLength({min: 10, max: 10}).isISO8601({strict: true}).optional({checkFalsy: true}),
+    check('order').isArray({min: 2})
+    ],
+    async (req, res) =>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }
+        const page = req.body;
+        let h=0, b=0;
+        const p = {
+            title:page.title,
+            author: page.author,
+            authorId: req.user.id,
+            creationDate: page.creationDate,
+            publicationDate: page.publicationDate,
+            ord: page.order.map((e)=>{
+                if(e.text){
+                    b++;
+                    return {
+                        text: e.text,
+                        pos: e.pos
+                    }
+                }
+                else if(e.header) {
+                    h++
+                    return {
+                        header: e.header,
+                        pos: e.pos
+                    }
+                }else{
+                    b++;
+                    return {
+                        id: e.id,
+                        pos: e.pos
+                    }
+                }
+            })
+        }
+        try {
+            if( h<1 || b<1) {
+                throw {err: "Number of block non right"};
+            }
+            if(req.user.username != p.author && req.user.administrator){
+                const aut = await userDao.getIdByUsername(p.author)
+                if(!aut.id)
+                    throw {error: "non-existent author"}
+                p.authorId = aut.id;
+            }
+            const id = await dao.addPage(p);
+            p.ord.map(async (e) => {
+                if (e.text) {
+                    await dao.addParagraph(e, id);
+                } else if(e.header){
+                    await dao.addHeader(e, id);
+                } else{
+                    await dao.addImage(e, id);
+                }
+            });
+            setTimeout(()=>res.status(200).json(id), answerDelay);
+        } catch(err) {
+            console.log(err);
+            if(err.error && err.error != "Number of block non right" && err.error != "non-existent author")
+                res.status(503).json({err: "Database error during the creation of page"});
+            res.status(422).json(err);
+        }
+});
 
 /** ******************************************************************************************************************************************* **/
 
@@ -91,4 +183,4 @@ app.delete('/api/sessions/current', (req, res) => {
 
 
 const PORT = 3001;
-app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
