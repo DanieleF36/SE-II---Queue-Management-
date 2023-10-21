261cf43131864e04c58d0c266ebd3c6de60deaf0
s317631 <s317631@studenti.polito.it>
2023-10-21 18:39:42 +0200

    Users View

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -9,14 +9,30 @@ import "./Login.css";
 function Homepage(props) {
 
   return (
-    <div className='background-image-container'>
-      <CustomNavbar loggedIn={props.loggedIn} user={props.user}/>
-      <Container className="d-flex align-items-center justify-content-center" style={{marginTop: '50px'}}>
-      <div>
-        <h1>Welcome OQM Website!!!</h1>
-      </div>
-    </Container>
+    props.user ? props.user.role === 'admin' ? <div className='background-image-container'>
+      <CustomNavbar loggedIn={props.loggedIn} user={props.user} />
+      <Container className="d-flex align-items-center justify-content-center" style={{ marginTop: '50px' }}>
+        <div>
+          <h1>Welcome OQM Website!!! - (Administrator View)</h1>
+        </div>
+      </Container>
     </div>
+      : <div className='background-image-container'>
+        <CustomNavbar loggedIn={props.loggedIn} user={props.user} />
+        <Container className="d-flex align-items-center justify-content-center" style={{ marginTop: '50px' }}>
+          <div>
+            <h1>Welcome OQM Website!!! - (Officer View)</h1>
+          </div>
+        </Container>
+      </div> :
+      <div className='background-image-container'>
+        <CustomNavbar loggedIn={props.loggedIn} user={props.user} />
+        <Container className="d-flex align-items-center justify-content-center" style={{ marginTop: '50px' }}>
+          <div>
+            <h1>Welcome OQM Website!!! - (Normal User View) </h1>
+          </div>
+        </Container>
+      </div>
   )
 }
 
